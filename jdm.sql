8a1ca1a3e7348fe4852c04ad76df6f11c8d37fcd
rahmannavid <rahmannavid@gmail.com>
2016-09-19 00:05:42 +0600

    new db

--- jdm.sql
+++ jdm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 09, 2016 at 07:29 PM
+-- Generation Time: Sep 17, 2016 at 07:56 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `category`
@@ -39,7 +39,30 @@ CREATE TABLE IF NOT EXISTS `category` (
 INSERT INTO `category` (`id`, `name`) VALUES
 (1, 'Car'),
 (2, 'Bike'),
-(3, 'Suspension');
+(3, 'Suspension'),
+(8, 'Engine');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_info`
+--
+
+CREATE TABLE IF NOT EXISTS `contact_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile_no` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `contact_info`
+--
+
+INSERT INTO `contact_info` (`id`, `company_name`, `address`, `email`, `mobile_no`) VALUES
+(1, 'Onnorokom Group', 'Mohakhali,Dhaka.', 'onnorokom@gmail.com', '+8800000000');
 
 -- --------------------------------------------------------
 
@@ -56,6 +79,10 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `comment` text,
   `status` int(11) DEFAULT NULL,
   `type` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `email` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -79,16 +106,17 @@ CREATE TABLE IF NOT EXISTS `product` (
   `datetime` datetime DEFAULT NULL,
   `auction` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 --
 -- Dumping data for table `product`
 --
 
 INSERT INTO `product` (`id`, `cata_id`, `name`, `description`, `price`, `condition`, `quantity`, `brand`, `country_manufacture`, `user_id`, `datetime`, `auction`) VALUES
-(1, 1, 'Car Engine ', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '50000', 1, '2', 'Toyota', 'Japan', 0, '0000-00-00 00:00:00', 1),
-(2, 2, 'Car Spoilar', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '10000', NULL, NULL, NULL, NULL, NULL, NULL, 2),
-(3, 3, 'CUSCO', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '40000', 2, '0', 'Cusco', 'Japan', NULL, NULL, 1);
+(1, 2, 'Car Engine ', '           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '50000', 1, '2', 'Toyota', 'Japan', 1, '2016-09-17 19:28:13', 1),
+(2, 2, 'Car Spoilar', '  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '10000', 2, '0', '', '', 1, '2016-09-17 07:43:30', 2),
+(3, 3, 'CUSCO', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '40000', 2, '0', 'Cusco', 'Japan', NULL, NULL, 1),
+(19, 8, 'Toyota Engine', ' Toyota Engine', '10000', 1, '100', 'Toyota', 'Japan', 1, '2016-09-17 08:51:40', 2);
 
 -- --------------------------------------------------------
 
@@ -125,15 +153,17 @@ CREATE TABLE IF NOT EXISTS `user` (
   `mobile` varchar(50) DEFAULT NULL,
   `type` int(11) NOT NULL,
   `password` varchar(20) NOT NULL,
+  `address` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `name`, `mobile`, `type`, `password`) VALUES
-(1, 'admin@jdm.com', 'admin', NULL, 1, '1234');
+INSERT INTO `user` (`id`, `email`, `name`, `mobile`, `type`, `password`, `address`) VALUES
+(1, 'admin@jdm.com', 'admin', NULL, 1, '1234', NULL),
+(2, 'rat@ratul.com', 'rat', '1222', 2, '1234', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
